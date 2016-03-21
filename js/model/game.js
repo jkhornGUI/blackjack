98c9d455d4b577063f7f03da19480a427a060fd6
Jerra Khorn <j.khorn1@gmail.com>
2016-03-20 23:23:12 -0400

    a

--- js/model/game.js
+++ js/model/game.js
@@ -1,7 +1,7 @@
 var game = {
 	numOfPlayers: 0,
 	initializeGame: function() {
-		view.setBoard( createBoard( numOfPlayers ) );
+		view.setBoard( createBoard( game.numOfPlayers ) );
 		view.showBoard();
 	},
 	setNumOfPlayers: function( newNum ) {
