f5a3aa031b0385ca5fd6680b8f6feffe39c5e5a7
Jerra Khorn <j.khorn1@gmail.com>
2016-03-20 21:32:54 -0400

    a

--- js/view/gameBoard.js
+++ js/view/gameBoard.js
@@ -1,9 +1,9 @@
 var initialBoardView = "<p><strong>How many players?</strong></p><br>" +
 			"<form id='howManyPlayers' class='form-inline' role='form' method='post'>" +
-				"<input type='radio' name='numOfPlayers' value='1' checked> 1&nbsp;&nbsp;" +
-				"<input type='radio' name='numOfPlayers' value='2'> 2&nbsp;&nbsp;" +
-				"<input type='radio' name='numOfPlayers' value='3'> 3&nbsp;&nbsp;" +
-				"<input type='radio' name='numOfPlayers' value='4'> 4&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='1' checked> 1&nbsp;&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='2'> 2&nbsp;&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='3'> 3&nbsp;&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='4'> 4&nbsp;&nbsp;&nbsp;" +
 				"<button type='submit' class='btn btn-default'>Start</button>" +
 			"</form>";
 
