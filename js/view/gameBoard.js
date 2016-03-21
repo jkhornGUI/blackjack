8e3ff57077b44f43434647e54232886346455915
Jerra Khorn <j.khorn1@gmail.com>
2016-03-20 22:20:50 -0400

    m

--- js/view/gameBoard.js
+++ js/view/gameBoard.js
@@ -1,6 +1,6 @@
-var initialBoardView = 	"<div class='form-group'>" +
+var initialBoardView = 	"<div class='form-group col-xs-2'>" +
 							"<label for='howManyPlayers'>Select list: How many players?</label>" +
-							"<select class='form-control col-sm-4' id='howManyPlayers'>" +
+							"<select class='form-control' id='howManyPlayers'>" +
 								"<option>1</option>" +
 								"<option>2</option>" +
 								"<option>3</option>" +
