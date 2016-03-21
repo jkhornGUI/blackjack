0c1535f899e48dc3652bb3130cf91d205bce2c5f
Jerra Khorn <j.khorn1@gmail.com>
2016-03-20 21:12:22 -0400

    edits

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,1 +1,8 @@
-var initialBoard = ""
+var initialBoard = "<p><strong>How many players?</strong></p><br>" +
+			"<form id='howManyPlayers' class='form-inline' role='form' method='post'>" +
+				"<input type='radio' name='numOfPlayers' value='1' checked> 1&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='2'> 2&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='3'> 3&nbsp;&nbsp;" +
+				"<input type='radio' name='numOfPlayers' value='4'> 4&nbsp;&nbsp;" +
+				"<button type='submit' class='btn btn-default'>Start</button>" +
+			"</form>";
