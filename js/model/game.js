41388adfa32a435534cff84581434eda86e7649d
Jerra Khorn <j.khorn1@gmail.com>
2016-03-20 21:23:13 -0400

    add edit

---
+++ js/model/game.js
@@ -0,0 +1,6 @@
+var gameObject = {
+	numOfPlayers: 0,
+	initializeGame: function() {
+		
+	}
+};
--- js/view/gameBoard.js
+++ js/view/gameBoard.js
@@ -1,4 +1,4 @@
-var initialBoard = "<p><strong>How many players?</strong></p><br>" +
+var initialBoardView = "<p><strong>How many players?</strong></p><br>" +
 			"<form id='howManyPlayers' class='form-inline' role='form' method='post'>" +
 				"<input type='radio' name='numOfPlayers' value='1' checked> 1&nbsp;&nbsp;" +
 				"<input type='radio' name='numOfPlayers' value='2'> 2&nbsp;&nbsp;" +
@@ -6,3 +6,9 @@ var initialBoard = "<p><strong>How many players?</strong></p><br>" +
 				"<input type='radio' name='numOfPlayers' value='4'> 4&nbsp;&nbsp;" +
 				"<button type='submit' class='btn btn-default'>Start</button>" +
 			"</form>";
+
+var viewObject = {
+	showBoard: function( board ) {
+		$( "#gameBoard" ).html( board );
+	}
+};
